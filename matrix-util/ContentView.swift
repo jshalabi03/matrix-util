57aee2d8cd1505378fb3cc52a49b9ce6e3ebd59b
jshalabi03 <jshalabi03@gmail.com>
2022-01-01 16:30:39 -0600

    Created initial matrix view struct.

--- matrix-util/ContentView.swift
+++ matrix-util/ContentView.swift
@@ -9,9 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Matrix(n: 3, m: 3)
     }
+}
+
+struct Matrix: View {
+    
+    private var n: Int
+    private var m: Int
+    
+    var body: some View {
+        
+        VStack {
+            ForEach(0..<n) { n in
+                HStack {
+                    ForEach(0..<m) { m in
+                        
+                        Text("\(n), \(m)")
+                            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                        
+                    }
+                }
+            }
+        }
+        
+    }
+    
+    init(n: Int, m: Int) {
+        self.n = n
+        self.m = m
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
