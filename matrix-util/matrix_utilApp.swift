72587121f6ba4f3ef14a56d9f7973c6f79a66fd4
jshalabi03 <jshalabi03@gmail.com>
2022-01-01 16:17:08 -0600

    Initial Commit

---
+++ matrix-util/matrix_utilApp.swift
@@ -0,0 +1,17 @@
+//
+//  matrix_utilApp.swift
+//  matrix-util
+//
+//  Created by Jacob Shalabi on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct matrix_utilApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ matrix-utilUITests/matrix_utilUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  matrix_utilUITestsLaunchTests.swift
+//  matrix-utilUITests
+//
+//  Created by Jacob Shalabi on 1/1/22.
+//
+
+import XCTest
+
+class matrix_utilUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
