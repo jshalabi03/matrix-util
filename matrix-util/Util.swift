23241a01dfcae9a5baaec801442a2635a080edce
jshalabi03 <jshalabi03@gmail.com>
2022-01-04 15:07:22 -0600

    Added function for determining invertibility.

--- matrix-util/Util.swift
+++ matrix-util/Util.swift
@@ -6,7 +6,12 @@
 //
 
 import Foundation
+import LASwift
 
 func roundToHundredths(_ num: Double) -> Double {
     return round(num * 100) / 100.0
+}
+
+func isInvertible(_ matrix: Matrix) -> Bool {
+    return matrix.cols == matrix.rows && det(matrix) == 0
 }
